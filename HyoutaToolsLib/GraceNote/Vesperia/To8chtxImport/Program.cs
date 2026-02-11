fb4671833253b856ce5f5637c9b3f4079e99c815
SlowPoke <57148919+Shizukage@users.noreply.github.com>
2026-02-11 20:36:04 +0700

    Auto-detect TO8CHTX layout in TXT workflow

--- HyoutaToolsLib/GraceNote/Vesperia/To8chtxImport/Program.cs
+++ HyoutaToolsLib/GraceNote/Vesperia/To8chtxImport/Program.cs
@@ -8,16 +8,31 @@ using HyoutaUtils;
 namespace HyoutaTools.GraceNote.Vesperia.TO8CHTXImport {
 	class Program {
 		public static int Execute( List<string> args ) {
-			if ( args.Count != 3 ) {
-				Console.WriteLine( "Usage: TO8CHTX_GraceNote ChatFilename NewDBFilename GracesJapanese" );
+			if ( args.Count != 3 && args.Count != 5 ) {
+				Console.WriteLine( "Usage: TO8CHTX_GraceNote ChatFilename NewDBFilename GracesJapanese [Encoding] [LanguageCount]" );
+				Console.WriteLine( "  Encoding: ShiftJIS or UTF8" );
 				return -1;
 			}
 
 			String Filename = args[0];
 			String NewDB = args[1];
 			String GracesDB = args[2];
+			TextUtils.GameTextEncoding encoding = TextUtils.GameTextEncoding.ShiftJIS;
+			int languageCount = 2;
 
-			ChatFile c = new ChatFile( Filename, EndianUtils.Endianness.BigEndian, TextUtils.GameTextEncoding.ShiftJIS, BitUtils.Bitness.B32, 2 );
+			if ( args.Count == 5 ) {
+				if ( !TryParseEncoding( args[3], out encoding ) ) {
+					Console.WriteLine( "Unknown encoding: " + args[3] );
+					return -1;
+				}
+
+				if ( !int.TryParse( args[4], out languageCount ) || languageCount < 2 ) {
+					Console.WriteLine( "LanguageCount must be an integer >= 2 for GraceNote import." );
+					return -1;
+				}
+			}
+
+			ChatFile c = new ChatFile( Filename, EndianUtils.Endianness.BigEndian, encoding, BitUtils.Bitness.B32, languageCount );
 
 			GraceNoteUtil.GenerateEmptyDatabase( NewDB );
 
@@ -40,6 +55,22 @@ namespace HyoutaTools.GraceNote.Vesperia.TO8CHTXImport {
 
 			GraceNoteDatabaseEntry.InsertSQL( Entries.ToArray(), "Data Source=" + NewDB, "Data Source=" + GracesDB );
 			return 0;
+		}
+
+		private static bool TryParseEncoding( string encodingString, out TextUtils.GameTextEncoding encoding ) {
+			switch ( encodingString.ToUpperInvariant() ) {
+				case "SHIFTJIS":
+				case "SJIS":
+					encoding = TextUtils.GameTextEncoding.ShiftJIS;
+					return true;
+				case "UTF8":
+				case "UTF-8":
+					encoding = TextUtils.GameTextEncoding.UTF8;
+					return true;
+				default:
+					encoding = TextUtils.GameTextEncoding.ShiftJIS;
+					return false;
+			}
 		}
 	}
 }
